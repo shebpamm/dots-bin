f5897b958819722008d1eae3c5a6405f1d26770a
Erik Karsten <erik@opcode.xyz>
2021-08-10 13:22:22 +0300

    changed to update only herbst colors

--- reload.sh
+++ reload.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-herbstclient reload
+source ~/.cache/wal/colors.sh
+
+~/.config/herbstluftwm/colors
+( xsettingsd & disown ) > /dev/null 2>&1
+cp ~/.cache/wal/colors* /srv/http/startpage/wal/
+#hass-hex light.living_room_strips $color1 > /dev/null 2>&1
 run-dunst.sh
+spicetify update > /dev/null 2>&1
